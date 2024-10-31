9ff82068040e99a7709956419fb488a14f2229d0
Robert Müller <robert.mueller@fatchip.de>
2024-10-31 10:17:05 +0100

    #0113880 - Removed iDeal issuer list

--- view/frontend/web/js/view/payment/method-renderer/ideal-method.js
+++ view/frontend/web/js/view/payment/method-renderer/ideal-method.js
@@ -6,32 +6,7 @@ define(
         'use strict';
         return Component.extend({
             defaults: {
-                template: 'Fatchip_Computop/payment/ideal',
-                idealIssuer: '',
-            },
-            initObservable: function () {
-                this._super()
-                    .observe([
-                        'idealIssuer'
-                    ]);
-                return this;
-            },
-            getData: function () {
-                var parentReturn = this._super();
-                if (parentReturn.additional_data === null) {
-                    parentReturn.additional_data = {};
-                }
-                parentReturn.additional_data.issuer = this.idealIssuer();
-                return parentReturn;
-            },
-            showIssuerList: function () {
-                if (window.checkoutConfig.payment.computop.computop_ideal.service === "direct") {
-                    return true;
-                }
-                return false;
-            },
-            getIdealIssuers: function () {
-                return window.checkoutConfig.payment.computop.computop_ideal.issuerList;
+                template: 'Fatchip_Computop/payment/ideal'
             }
         });
     }
