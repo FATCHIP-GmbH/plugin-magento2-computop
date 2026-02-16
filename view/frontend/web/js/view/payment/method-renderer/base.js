ee0b3ff05c2ff158758502822e64f617a9c4b5c6
Robert Müller <robert.mueller@fatchip.de>
2026-02-16 17:43:02 +0100

    FC#0128783 - Added javascript mixin to prevent an unnecessary request leading to a session race condition

--- view/frontend/web/js/view/payment/method-renderer/base.js
+++ view/frontend/web/js/view/payment/method-renderer/base.js
@@ -66,6 +66,8 @@ define(
             handleRedirectAction: function(url) {
                 var self = this;
 
+                window.computopRedirectInitiated = true;
+
                 this.isPlaceOrderActionAllowed(false);
                 this.getPlaceOrderDeferredObject()
                     .fail(
